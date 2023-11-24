ad242ec5bfd8f04c53c8461308bb6ac7e40563ea
abulacode <128340763+abulacode@users.noreply.github.com>
2023-11-24 16:07:40 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/XiaoYiChaHang/xxzb/raw/branch/master/hccx.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
